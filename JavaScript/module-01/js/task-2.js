7b472a8407b00c20d219630dc7a62437b3daa39b
Aleksandr <olexander.o.havrylenko@gmail.com>
2019-07-15 10:34:35 +0300

    added module-01

---
+++ JavaScript/module-01/js/task-2.js
@@ -0,0 +1,15 @@
+// let total = 100;
+// let ordered = 200;
+
+
+// console.log(`ordered > total`, ordered > total);
+// console.log(`ordered < total`, ordered < total);
+
+// ordered = 100;
+// total = 200;
+
+// if(ordered > total) {
+//     alert(`"На складе недостаточно товаров!"`);
+// } else if(ordered < total) {
+//     alert(`"Заказ оформлен, с вами свяжется менеджер"`);
+// }
